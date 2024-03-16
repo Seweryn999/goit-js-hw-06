e87891fd8a4e27f898aeccf4a3c502004ade90b2
Seweryn <sewcio0212@gmail.com>
2024-03-16 09:43:34 +0100

    toTask9-done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (!emailInput.value || !passwordInput.value) {
+    alert("Wypełnij wszystkie pola!");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log("Dane z formularza:", formData);
+
+    emailInput.value = "";
+    passwordInput.value = "";
+  }
+});
