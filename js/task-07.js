0517e67481fa824d75deaa0bf60f8afa03ef05cc
Seweryn <sewcio0212@gmail.com>
2024-03-18 12:20:00 +0100

    +const initialSize

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,9 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const textSpan = document.querySelector("#text");
+const initialSize = (textSpan.style.fontSize = fontSizeControl.value + "px");
 
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
+
   textSpan.style.fontSize = fontSize;
 });
